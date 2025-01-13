e376048900fb5605122c7a0b2a4c85c582bc5409
wgshuo <wgshuo_1990@163.com>
2025-01-13 21:36:43 +0800

    Site updated: 2025-01-13 21:36:42

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/12/这是一篇新的文章/","2025/01/12/hello-world/"];function toRandomPost(){
+var posts=["2025/01/12/hello-world/","2025/01/12/这是一篇新的文章/","2025/01/13/这是另一篇新的博文/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
