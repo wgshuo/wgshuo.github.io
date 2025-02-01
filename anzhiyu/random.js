1f6895665b1976cdde90c4f47040c92fb62b02f8
wgshuo <wgshuo_1990@163.com>
2025-02-01 16:51:53 +0800

    Site updated: 2025-02-01 16:51:52

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["2025/02/01/一篇文章/","2025/01/22/电脑已连接WiFi但无法上网/","2025/01/20/Git连接Github超时的解决方法/","2025/01/20/hello-world/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
