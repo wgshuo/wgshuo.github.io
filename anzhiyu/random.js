14f0c59324c12ca8083831ea699910e10961670f
wgshuo <wgshuo_1990@163.com>
2025-02-09 13:00:37 +0800

    Site updated: 2025-02-09 13:00:37

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/01/零成本搭建个人博客：Hexo-GitHub-Cloudflare/","2025/02/01/hexo部署出现错误Spawn failed解决方法/","2025/02/01/IObit Uninstaller PRO 14卸载软件工具/","2025/01/22/电脑已连接WiFi但无法上网/","2025/01/20/Git连接Github超时的解决方法/","2025/01/20/hello-world/"];function toRandomPost(){
+var posts=["2025/02/08/IDM下载神奇终极指南/","2025/02/01/零成本搭建个人博客：Hexo-GitHub-Cloudflare/","2025/02/01/hexo部署出现错误Spawn failed解决方法/","2025/02/01/IObit Uninstaller PRO 14卸载软件工具/","2025/01/22/电脑已连接WiFi但无法上网/","2025/01/20/Git连接Github超时的解决方法/","2025/01/20/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
